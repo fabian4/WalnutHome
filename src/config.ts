a85c41b6f1e7b7bc0f8bbdb3dde76a703ac9d8b0
fabian4 <fabian4@163.com>
2022-03-01 13:53:50 +0800

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const Config = {
+    BotName: '5GBot',
+    WECHATY_PUPPET_SERVICE_TOKEN: 'puppet_padlocal_bf8677631e3546ed95a923e8a38f6d6b',
+}
+
+export default Config
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import {PuppetPadlocal} from "wechaty-puppet-padlocal";
+import {Contact, Message, ScanStatus, WechatyBuilder} from "wechaty";
+import Config from "./config";
+
+const token: string = Config.WECHATY_PUPPET_SERVICE_TOKEN            // padlocal token
+const puppet = new PuppetPadlocal({ token })
+
+const bot = WechatyBuilder.build({
+    name: Config.BotName,
+    puppet: puppet,
+})
+
+bot.on("scan", (qrcode: string, status: ScanStatus) => {
+        if (status === ScanStatus.Waiting && qrcode) {
+            const qrcodeImageUrl = ["https://api.qrserver.com/v1/create-qr-code/?data=", encodeURIComponent(qrcode)].join("");
+            console.log(`onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
+        } else {
+            console.log(`onScan: ${ScanStatus[status]}(${status})`);
+        }
+    })
+
+// bot.on("login", (user: Contact) => {
+//         console.log(`${user} login`);
+//     })
+//
+// bot.on("logout", (user: Contact) => {
+//         console.log(`${user} logout`);
+//     })
+
+bot.on("message", async (message: Message) => {
+        console.log(`on message: ${message.toString()}`);
+    })
+
+bot.start()
+
+console.log(Config.BotName, "started");
