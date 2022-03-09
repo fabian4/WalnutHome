cd325df71d7c8e18194925f89e433db17feff65d
fabian4 <fabian4@163.com>
2022-03-09 09:05:09 +0800

    feat: add chat group

--- src/message.ts
+++ src/message.ts
@@ -1,16 +1,20 @@
-import {log, Message} from "wechaty";
+import {Contact, log, Message} from "wechaty";
 import Config from "./config";
 
 export default async function onMessage(msg: Message) {
     if (msg.room()) {
         return
     }
-    if (Config.GROUP.indexOf(msg.talker().id) === -1) {
+    const id = msg.talker().id
+    if (Config.GROUP.indexOf(id) === -1) {
         return
     }
     log.info(Config.BotName, msg.text())
-
-    if (msg.text() === 'ding') {
-        await msg.say('dong')
+    for (let i = 0; i < Config.GROUP.length; i++) {
+        if(Config.GROUP[i] === id){
+            continue;
+        }
+        const contact: Contact = await Contact.find(Config.GROUP[i])
+        await contact.say(msg.id + ': ' + msg.text())
     }
 }
