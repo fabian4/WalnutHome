28b4d7eaba2cfbcef25762afdb926ac77567963f
fabian4 <fabian4@163.com>
2022-03-09 08:59:58 +0800

    feat: add chat group

--- src/message.ts
+++ src/message.ts
@@ -2,7 +2,10 @@ import {log, Message} from "wechaty";
 import Config from "./config";
 
 export default async function onMessage(msg: Message) {
-    if(msg.room()){
+    if (msg.room()) {
+        return
+    }
+    if (Config.GROUP.indexOf(msg.talker().id) === -1) {
         return
     }
     log.info(Config.BotName, msg.text())
