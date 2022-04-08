bfeb287751bee69caf2a445ffb21db748198005a
fabian4 <fabian4@163.com>
2022-04-08 16:36:21 +0800

    feat: switch puppet to walnut

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const Config = {
     BotName: '5GBot',
     WECHATY_PUPPET_SERVICE_TOKEN: 'puppet_padlocal_bf8677631e3546ed95a923e8a38f6d6b',
-    GROUP: ['111', "222"]
+    GROUP: ['15751763183', "13063824001"]
 }
 
 export default Config
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import {Contact, log, WechatyBuilder} from "wechaty";
+import {Contact, log, Message, WechatyBuilder} from "wechaty";
 import Config from "./config";
-import onMessage from "./message";
 import PuppetWalnut from "wechaty-puppet-walnut";
 
 const puppet = new PuppetWalnut()
@@ -10,15 +9,6 @@ const bot = WechatyBuilder.build({
     puppet
 })
 
-// bot.on("scan", async (qrcode: string, status: ScanStatus) => {
-//         if (status === ScanStatus.Waiting && qrcode) {
-//             const qrcodeImageUrl = ["https://api.qrserver.com/v1/create-qr-code/?data=", encodeURIComponent(qrcode)].join("");
-//             console.log(`onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
-//         } else {
-//             console.log(`onScan: ${ScanStatus[status]}(${status})`);
-//         }
-//     })
-
 bot.on("login", (user: Contact) => {
         console.log(`${user} login`);
     })
@@ -27,7 +17,23 @@ bot.on("logout", (user: Contact) => {
         console.log(`${user} logout`);
     })
 
-bot.on("message", onMessage)
+bot.on("message", async (msg: Message) => {
+    if (msg.room()) {
+        return
+    }
+    const id = msg.talker().id
+    if (Config.GROUP.indexOf(id) === -1) {
+        return
+    }
+    log.info(Config.BotName, msg.text())
+    for (let i = 0; i < Config.GROUP.length; i++) {
+        if (Config.GROUP[i] === id) {
+            continue;
+        }
+        const contact = await bot.Contact.find({id: Config.GROUP[i]})
+        await contact!.say(msg.talker().id + ': ' + msg.text())
+    }
+})
 
 bot.start()
     .then(() => log.info('StarterBot', 'Starter Bot Started.'))
